fea9531a8c8ef34184c8ef72ff280fba38c19952
Emmanuel <emmanueljovanovic40@gmail.com>
2022-10-17 20:46:47 +0200

    base

--- MainWindow.cs
+++ MainWindow.cs
@@ -47,9 +47,6 @@ namespace WeatherAPI
         private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
         {
             builder.Autoconnect(this);
-
-            DeleteEvent += Window_DeleteEvent;
-            _button1.Clicked += Button1_Clicked;
         }
 
         private void Window_DeleteEvent(object sender, DeleteEventArgs a)
@@ -57,7 +54,7 @@ namespace WeatherAPI
             Application.Quit();
         }
 
-        private void Button1_Clicked(object sender, EventArgs a)
+        private void Search_Clicked(object sender, EventArgs a)
         {
             _label1.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
         }
