32577b89e7cfd58bf5439f1708047d3ff2884f51
Emmanuel <emmanueljovanovic40@gmail.com>
2022-10-17 20:45:42 +0200

    base

--- MainWindow.cs
+++ MainWindow.cs
@@ -4,6 +4,31 @@ using UI = Gtk.Builder.ObjectAttribute;
 
 namespace WeatherAPI
 {
+     class WeatherInfo
+    {
+        public class coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+        public class weather{
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+        public class main
+        {
+            public double temp { get; set; }
+            public double humidity { get; set; }
+        }
+        public class root
+        {
+            public coord coord { get; set; }
+            public List <weather> weather { get; set; }
+            public main main { get; set; }
+        }
+        
+    }
     public class MainWindow : Window
     {
         [UI] private Search _search = null;
